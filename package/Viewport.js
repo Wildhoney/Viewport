d82ae4861fa565e2dc634e94fdbba82f5bd90b00
Adam Timberlake <adam.timberlake@gmail.com>
2014-01-30 00:05:36 +0000

    Added basic example

--- package/Viewport.js
+++ package/Viewport.js
@@ -5,9 +5,28 @@
         /**
          * @method analyse
          * @param element {Object}
-         * @return {Number}
+         * @return {Object}
          */
         analyse: function analyse(element) {
+
+            // Initialise the variables.
+            var yAxis = 0, xAxis = 0;
+
+            if (element.offsetTop < 0) {
+
+                // Deduct the `offsetTop` if it's less than zero.
+                yAxis += element.offsetTop;
+
+            }
+
+            if (element.offsetLeft < 0) {
+
+                // Deduct the `offsetLeft` if it's less than zero.
+                xAxis += element.offsetLeft;
+
+            }
+
+            return { y: yAxis, x: xAxis };
 
         }
 
